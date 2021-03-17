7f4027e30443d6110f1eb72cedc8bfee25dad961
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-03-17 16:57:47 +0100

    match_method added inside recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,19 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
   has_many :reviews
+
+  def self.match(ingredients)
+    recipes = Recipe.joins(:recipe_ingredients).where(recipe_ingredients: { ingredient_id: ingredients })
+    recipes = recipes.map { |recipe| recipe.id }
+    priorities = recipes.uniq.map { |recipe| { recipe: Recipe.find_by(id: recipe), matching_ingredients: recipes.count(recipe) } }
+    priorities = priorities.map do |hash|
+      {
+        matching_ingredients: hash[:matching_ingredients],
+        recipes: priorities.map do |recipe|
+          recipe[:recipe] if recipe[:matching_ingredients] == hash[:matching_ingredients]
+        end.compact
+      }
+    end.uniq
+    priorities.sort_by { |hash| -hash[:matching_ingredients] }
+  end
 end
