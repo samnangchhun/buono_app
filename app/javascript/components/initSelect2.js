447ee64a56f67b6f8f0dfa7264dc99eb6e9500f1
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-03-25 16:11:22 +0100

    fridge titles fixed

--- app/javascript/components/initSelect2.js
+++ app/javascript/components/initSelect2.js
@@ -2,11 +2,24 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2({tags: true, width: "100%"})
-  .on("select2:unselecting", function(e) {
-    var self = $('.select2');
+  const $searchBar = $('.select2').select2({tags: true, width: "100%",allowClear: true, placeholder: "" })
+  // .on("select2:unselecting", function(e) {
+    // var self = $('.select2');
+    // setTimeout(function() {
+    //   self.select2('close');
+    // }, 0);
+  // });
+  $searchBar.on("select2:selecting", function(e) {
+    const title = document.querySelector('.fridge-title');
+    title.innerHTML = `Shake your phone`;
+  });
+  $searchBar.on("select2:clear", function(e) {
+    const title = document.querySelector('.fridge-title');
+    title.innerHTML = `Your fridge is empty`;
+  });
+  $searchBar.on("select2:unselect", function(e) {
     setTimeout(function() {
-      self.select2('close');
+      $searchBar.select2('close');
     }, 0);
   });
 }
