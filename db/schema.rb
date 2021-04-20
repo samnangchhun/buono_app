a252701a8a4cdf5412021b3c56c0c1c2f1b1b79a
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-04-20 10:16:56 +0200

    cooking time from float to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_082917) do
+ActiveRecord::Schema.define(version: 2021_04_19_144528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,11 +64,11 @@ ActiveRecord::Schema.define(version: 2021_04_19_082917) do
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "instruction"
-    t.float "cooking_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo"
     t.string "doses"
+    t.integer "cooking_time"
   end
 
   create_table "reviews", force: :cascade do |t|
