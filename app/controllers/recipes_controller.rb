b644e2666c95e15d1f8a37cfb46c3457bc376d7b
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-04-20 10:18:57 +0200

    design changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,7 @@ class RecipesController < ApplicationController
       @recipes = Recipe.match(params[:ingredients])
       # .map { |recipe| recipe[:recipe] }
     else
-      @recipes = Recipe.all.sort_by { |recipe| recipe.cooking_time }
+      @recipes = Recipe.all.sort_by { |recipe| recipe.ingredients.length }
     end
   end
 
