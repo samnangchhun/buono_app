b32202254b70d6b1152100fa15b7edc92d2461b0
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-04-21 14:05:33 +0200

    seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,46 +2,46 @@
 require 'json'
 require 'open-uri'
 
-url = "https://api.spoonacular.com/recipes/random?number=100&apiKey=#{ENV['SPOONACULAR_KEY']}"
+# url = "https://api.spoonacular.com/recipes/random?number=100&apiKey=#{ENV['SPOONACULAR_KEY']}"
 
-filepath = 'recipes.json'
-serialized_present_recipe = File.read(filepath)
-present_recipe = JSON.parse(serialized_present_recipe)
+# filepath = 'db/recipes.json'
+# serialized_present_recipe = File.read(filepath)
+# present_recipe = JSON.parse(serialized_present_recipe)
 
-json_api = open(url).read
-result_api = JSON.parse(json_api)
+# json_api = open(url).read
+# result_api = JSON.parse(json_api)
 
-new_recipes = result_api['recipes'].map do |recipe|
-  next if recipe['instructions'].nil? || recipe['image'].nil? || recipe['analyzedInstructions'].first.nil? || recipe['extendedIngredients'].nil?
+# new_recipes = result_api['recipes'].map do |recipe|
+#   next if recipe['instructions'].nil? || recipe['image'].nil? || recipe['analyzedInstructions'].first.nil? || recipe['extendedIngredients'].nil? || recipe.nil?
 
-  if !recipe['analyzedInstructions'].empty?
-    steps = recipe['analyzedInstructions'][0]['steps'].map { |e| e['step'] }
-  end
+#   if !recipe['analyzedInstructions'].empty?
+#     steps = recipe['analyzedInstructions'][0]['steps'].map { |e| e['step'] }
+#   end
 
-  next if steps.include?('Email')
+#   next if steps.include?('Email')
 
-  if !recipe['extendedIngredients'].empty?
-    ingredients = recipe['extendedIngredients'].map { |ing| ing['name'] }
-    doses = recipe['extendedIngredients'].map { |ing| ing['original'] }
-  end
+#   if !recipe['extendedIngredients'].empty?
+#     ingredients = recipe['extendedIngredients'].map { |ing| ing['name'] }
+#     doses = recipe['extendedIngredients'].map { |ing| ing['original'] }
+#   end
 
-  {
-    title: recipe['title'],
-    image: recipe['image'],
-    cooking_time: recipe['readyInMinutes'],
-    instruction: steps,
-    ingredients: ingredients.uniq,
-    doses: doses,
-  }
-end
+#   {
+#     title: recipe['title'],
+#     image: recipe['image'],
+#     cooking_time: recipe['readyInMinutes'],
+#     instruction: steps,
+#     ingredients: ingredients.uniq,
+#     doses: doses,
+#   }
+# end
 
-new_recipes.each do |recipe|
-  present_recipe << recipe unless present_recipe.include?(recipe)
-end
+# new_recipes.each do |recipe|
+#   present_recipe << recipe unless present_recipe.include?(recipe)
+# end
 
-File.open(filepath, 'wb') do |file|
-  file.write(JSON.generate(present_recipe))
-end
+# File.open(filepath, 'wb') do |file|
+#   file.write(JSON.generate(present_recipe))
+# end
 
 
 puts 'seeeding recipes'
