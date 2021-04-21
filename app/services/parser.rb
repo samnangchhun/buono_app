ba537a9da12f9f3e8a9cdd2364b8760b5c10e832
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-04-21 10:57:01 +0200

    fetch.rb deleted

--- app/services/parser.rb
+++ app/services/parser.rb
@@ -1,31 +1,4 @@
-# require 'json'
 
-# class Parser
-  # def self.recipes
-  #   recipe_filepath = '../db/recipe.json'
-  #   serialized_recipes = File.read(recipe_filepath)
-  #   JSON.parse(serialized_recipes)
-  # end
-
-  # def self.parse_json
-  #   apikey = "c230327f3f7043b1b2990425b5e59e12"
-  #   recipes_filepath = File.join(__dir__, "../../db/jsons/recipes.json")
-  #   serialized_recipes = File.read(recipes_filepath)
-  #   recipes = JSON.parse(serialized_recipes)
-  #   id_recipes = recipes.map { |id_recipe| id_recipe['id'] }
-  #   id_recipes.each do |id|
-  #     url_ingredients = "https://api.spoonacular.com/recipes/#{id}/ingredientWidget.json?apiKey=#{apikey}"
-  #     Fetch.fetch(url_ingredients, id, "ingredients")
-  #   end
-  # end
-
-  # def self.parse_json(filepath)
-  #   JSON.parse(File.read(filepath))
-  # end
-
-  # url_recipes = "https://api.spoonacular.com/recipes/#{id}/information?apiKey=#{apikey}&includeNutrition=false"
-  # url_ingredients = "https://api.spoonacular.com/recipes/#{id}/ingredientWidget.json?apiKey=#{apikey}"
-# end
 require 'json'
 require 'open-uri'
 
