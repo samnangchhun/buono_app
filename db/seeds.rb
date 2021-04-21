33f3e0d735cc9f63da7ced164e2e6e04410383d3
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-04-21 12:15:21 +0200

    service folder deleted

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,48 @@
 # require 'faker'
 require 'json'
+require 'open-uri'
+
+url = "https://api.spoonacular.com/recipes/random?number=100&apiKey=#{ENV['SPOONACULAR_KEY']}"
+
+filepath = '../../db/recipes.json'
+serialized_present_recipe = File.read(filepath)
+present_recipe = JSON.parse(serialized_present_recipe)
+
+json_api = open(url).read
+result_api = JSON.parse(json_api)
+
+new_recipes = result_api['recipes'].map do |recipe|
+  next if recipe['instructions'].nil? || recipe['image'].nil? || recipe['analyzedInstructions'].first.nil? || recipe['extendedIngredients'].nil?
+
+  if !recipe['analyzedInstructions'].empty?
+    steps = recipe['analyzedInstructions'][0]['steps'].map { |e| e['step'] }
+  end
+
+  next if steps.include?('Email')
+
+  if !recipe['extendedIngredients'].empty?
+    ingredients = recipe['extendedIngredients'].map { |ing| ing['name'] }
+    doses = recipe['extendedIngredients'].map { |ing| ing['original'] }
+  end
+
+  {
+    title: recipe['title'],
+    image: recipe['image'],
+    cooking_time: recipe['readyInMinutes'],
+    instruction: steps,
+    ingredients: ingredients.uniq,
+    doses: doses,
+  }
+end
+
+new_recipes.each do |recipe|
+  present_recipe << recipe unless present_recipe.include?(recipe)
+end
+
+File.open(filepath, 'wb') do |file|
+  file.write(JSON.generate(present_recipe))
+end
+
 puts 'seeding 5 users...'
 
 User.create(email: 'fra@mail.com', password: '123456')
@@ -10,37 +53,11 @@ User.create(email: 'lara@mail.com', password: '123456')
 
 puts 'users created'
 
-# # puts 'seeding 20 ingredients...'
-
-# # 20.times do
-# #   Ingredient.create(name: Faker::Food.unique.ingredient)
-# # end
-
-# puts 'ingredients created'
-
-# puts 'seeding 10 recipes...'
-  # Recipe.create(
-  #   title: Faker::Food.unique.dish,
-  #   instruction: Faker::Food.description,
-  #   cooking_time: rand(10..40)
-  # )
-# Dir.each_child('db/jsons/recipes') do |file|
-#   json = JSON.parse(file)
-#   recipe = Recipe.new
-#   recipe.title = json['title']
-#   recipe.cooking_time = json['readyInMinutes']
-#   recipe.photo = json['image']
-#   recipe.instruction = json['instructions']
-#   p recipe
-# end
 
 puts 'seeeding recipes'
 serialized_recipes = File.read("db/recipes.json")
 recipes = JSON.parse(serialized_recipes)
-# p recipes.class
-# p recipes.first['instruction']
-# ingredients = Dir["db/jsons/ingredients/**/*.json"].sort
-# recipes = Dir["db/jsons/recipes/**/*.json"].sort
+
 
 recipes.each do |recipe_json|
   next if !Recipe.find_by(title: recipe_json['title']).nil?
@@ -73,44 +90,3 @@ end
 
 puts 'recipes created'
 
-# puts 'joining ingredients and recipe...'
-
-# Recipe.all.each do |recipe|
-#   ingredients = Ingredient.all.sample(rand(1..4))
-#   ingredients.each do |ingredient|
-#     RecipeIngredient.create(recipe: recipe, ingredient: ingredient)
-#   end
-# end
-
-# puts 'joining done'
-
-# puts 'seeding some reviews'
-
-# Recipe.all[0..2].each_with_index do |recipe, index|
-#   (index + 1).times do
-#     Review.create(
-#       rating: rand(1..5),
-#       content: Faker::Restaurant.unique.review,
-#       recipe: recipe,
-#       user: User.all.sample
-#     )
-#   end
-# end
-
-# puts 'reviews done'
-
-# puts 'seeding some bookmarks...'
-
-# User.all[0..2].each_with_index do |user, index|
-#   (index + 1).times do
-#     bookmark = Bookmark.new(user: user, recipe: Recipe.all.sample)
-#     until bookmark.save
-#       bookmark.recipe = Recipe.all.sample
-#     end
-#   end
-# end
-
-# puts 'bookmarks done'
-
-
-# puts 'seeds completed'
